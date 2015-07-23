8b6e59bd88a3cdc97ec786fc28fd923bd8914345
Dan Harper <intouch@danharper.me>
2015-07-23 23:46:54 +0100

    cl?

--- src/List/createListActions.js
+++ src/List/createListActions.js
@@ -5,49 +5,55 @@ export default function createListActions(config) {
 
   const { REQUEST_LIST_ACTION, CHANGE_FILTER_ACTION } = actionTypes(config.actionTypeNamespace)
 
+  return {
+    filterList,
+    nextPage,
+    previousPage: prevPage,
+    goToPage,
+  }
 
-  const fetchList = (dispatch, getState) => {
+  function fetchList(dispatch, getState) {
     dispatch({
       types: REQUEST_LIST_ACTION,
       payload: FETCH_LIST({ query: getState()[STORE_NAME].filters })
     })
   }
 
-  const filterList = (filter, value) => dispatch => {
-    dispatch({
-      type: CHANGE_FILTER_ACTION,
-      payload: { [filter]: value }
-    })
+  function filterList(filter, value) {
+    return dispatch => {
+      dispatch({
+        type: CHANGE_FILTER_ACTION,
+        payload: { [filter]: value }
+      })
 
-    dispatch(fetchList)
+      dispatch(fetchList)
+    }
   }
 
-  const fetchPage = pageChanger => (dispatch, getState) => {
-    let currentPage = getState()[STORE_NAME].filters.page
-    let nextPage = pageChanger(currentPage)
-    dispatch(filterList('page', nextPage))
+  function fetchPage(pageChanger) {
+    return (dispatch, getState) => {
+      let currentPage = getState()[STORE_NAME].filters.page
+      let nextPage = pageChanger(currentPage)
+      dispatch(filterList('page', nextPage))
+    }
   }
 
-  const nextPage = () => dispatch => {
-    dispatch(fetchPage(currentPage => currentPage + 1))
+  function nextPage() {
+    return dispatch => {
+      dispatch(fetchPage(currentPage => currentPage + 1))
+    }
   }
 
-  const prevPage = () => dispatch => {
-    dispatch(fetchPage(currentPage => currentPage - 1))
+  function prevPage() {
+    return dispatch => {
+      dispatch(fetchPage(currentPage => currentPage - 1))
+    }
   }
 
-  const goToPage = page => dispatch => {
-    dispatch(fetchPage(() => page))
-  }
-
-
-
-  return {
-    /* action creators.. */
-    filterList,
-    nextPage,
-    previousPage: prevPage,
-    goToPage,
+  function goToPage(page) {
+    return dispatch => {
+      dispatch(fetchPage(() => page))
+    }
   }
 
 }
