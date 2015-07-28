260c33fd11b4968ac67a0683f19fb51239c601f4
Dan Harper <intouch@danharper.me>
2015-07-28 23:50:22 +0100

    router!

--- src/App.js
+++ src/App.js
@@ -3,14 +3,52 @@ import { provide } from 'react-redux'
 import store from './store'
 import { InspectionsList } from './Inspections/InspectionsList'
 import { PropertiesList } from './Properties/PropertiesList'
+import { reduxRouteComponent } from 'redux-react-router'
+import { Router, Route, Redirect, Link } from 'react-router'
+import { history } from 'react-router/lib/BrowserHistory'
 
 window.__s = () => store.getState()
 
-@provide(store)
+class Container extends Component {
+  render() {
+    return (
+      <main className="app">
+        <nav className="app__nav">
+          <ul>
+            <li><Link to="home">Home</Link></li>
+            <li><Link to="properties">Properties</Link></li>
+            <li><Link to="inspections">Inspections</Link></li>
+          </ul>
+        </nav>
+        <section className="app__main">
+          {this.props.children}
+        </section>
+      </main>
+    )
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <p>Hello :)</p>
+    )
+  }
+}
+
 export default class App extends Component {
   render() {
-    // return <div>:D</div>
-    // return <InspectionsList />
-    return <PropertiesList />
+    return (
+      <Router history={history}>
+        <Route component={reduxRouteComponent(store)}>
+          <Route component={Container}>
+            <Redirect from="/" to="home" />
+            <Route path="home" component={Home} />
+            <Route path="properties" component={PropertiesList} />
+            <Route path="inspections" component={InspectionsList} />
+          </Route>
+        </Route>
+      </Router>
+    )
   }
 }
--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,5 @@
+export { routerStateReducer as router } from 'redux-react-router'
+
 export * from './Properties/PropertiesList/reducers'
 export * from './Inspections/InspectionsList/reducers'
 export * from './Filters/FilterInputs/ClientsSelectFilter/reducers'
