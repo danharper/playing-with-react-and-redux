ea25f44b308c037b84a45e28428c6065260a2bfa
Dan Harper <intouch@danharper.me>
2015-07-28 20:55:55 +0100

    helper to view state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import store from './store'
 import { InspectionsList } from './Inspections/InspectionsList'
 import { PropertiesList } from './Properties/PropertiesList'
 
+window.__s = () => store.getState()
+
 @provide(store)
 export default class App extends Component {
   render() {
