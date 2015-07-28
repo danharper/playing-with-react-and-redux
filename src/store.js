0a65c74932c1a3e85e173df28353a9dda417db1d
Dan Harper <intouch@danharper.me>
2015-07-28 20:55:42 +0100

    compose store, looks nicer

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import * as reducers from './reducers'
 import middleware from './middleware'
 
 console.debug('REDUCERS', reducers)
 
-const createAppStore = applyMiddleware(...middleware)(createStore)
+const finalCreateStore = compose(
+  applyMiddleware(...middleware),
+  createStore
+)
 
-export default createAppStore(combineReducers(reducers))
+export default finalCreateStore(
+  combineReducers(reducers)
+)
